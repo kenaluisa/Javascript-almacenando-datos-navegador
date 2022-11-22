057a2b308822c9b82afafa0a246226639f11953a
kenaluisa <matzenbacherkena@gmail.com>
2022-11-22 14:41:53 -0300

    finalizando el proyecto

--- components/readTasks.js
+++ components/readTasks.js
@@ -1,12 +1,13 @@
 import { createTask } from "./addTask.js";
 import {dateElement} from "./dateElement.js";
-import { uniqueDates } from "../services/date.js";
+import { uniqueDates, orderDates } from "../services/date.js";
 
 export const displayTask= () =>{
     const list = document.querySelector('[data-list] ');
     const taskList = JSON.parse( localStorage.getItem('tasks')) || [];
     const dates = uniqueDates(taskList);
- 
+    orderDates(dates);
+    
     dates.forEach(date => {
         const dateMoment = moment(date, 'DD/MM/YYYY');
         list.appendChild(dateElement(date));
